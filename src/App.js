11dc7087cceac250751408c301df278f447b4eb6
SalTor <sal@saltor.nyc>
2020-05-12 14:53:16 -0400

    Initial grid rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const size = 3
+const initialGrid = Array(size).fill(Array(size).fill(0))
+/*
+    [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+*/
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [grid, updateGrid] = useState(initialGrid)
+    const [winnerExists, setWinnerExists] = useState(false)
+
+    const move = () => {}
+
+    return (
+        <div className="App">
+            <header>
+                <h1>Tic Tac Toe</h1>
+            </header>
+
+            <div className="grid">
+                {grid.map((row, rowIndex) => (
+                    <div className="grid__row" key={`row_${rowIndex}`}>
+                        {row.map((col, colIndex) => (
+                            <button
+                                className="grid__tile"
+                                disabled={winnerExists}
+                                onClick={() => winnerExists || move(rowIndex, colIndex)}
+                                key={`tile_${colIndex}`}
+                                type="button"
+                            >{col}</button>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
